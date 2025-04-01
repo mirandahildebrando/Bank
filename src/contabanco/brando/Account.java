ac48c3fe678a84cbe4edf387c0ed504af06956eb
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-03-31 22:30:17 -0300

    creating the account class and organizing the objects

---
+++ src/contabanco/brando/Account.java
@@ -0,0 +1,14 @@
+package contabanco.brando;
+
+public class Account {
+	
+	private String ag;
+	private String cc;
+	private String name;
+	
+	public Account(String ag, String cc, String name) {
+		this.ag = ag;
+		this.cc = cc;
+		this.name = name;
+	}
+}
--- src/contabanco/brando/App.java
+++ src/contabanco/brando/App.java
@@ -4,6 +4,19 @@ public class App {
 	
 	public static void main(String[] args) {
 		System.out.println("Ola mundo");
+		
+		
+		//Criar uma conta (agencia,conta nome)
+		Account account = new Account(ag: "0001", cc: "1234", name: "Brando");
+		
+		
+		//Limite maximo de nome 12 caracteres
+		//Sacar valores
+		//Não pode sacar mais do que tem
+		//Depositar
+		//Informar ao usuário as operações
+		
+		
 	}
 
 }
