4f5191335ce4b431f9d99c0f1b11ee5b8820215e
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-04-08 17:04:05 -0300

    implementing the withdraw

--- src/contabanco/brando/Account.java
+++ src/contabanco/brando/Account.java
@@ -5,10 +5,34 @@ public class Account {
 	private String ag;
 	private String cc;
 	private String name;
+	 
+	 private double balance;
+	
+	private static final int MAX_LENGTH = 12;
 	
 	public Account(String ag, String cc, String name) {
 		this.ag = ag;
 		this.cc = cc;
-		this.name = name;
+		setName(name);
+	}
+	
+	public void setName(String name) {
+		if(name.length() > MAX_LENGTH) {
+			this.name = name.substring(0, MAX_LENGTH);
+		} else {
+			this.name = name;
+		}
+		System.out.println(this.name);
+	}
+	
+	public boolean withDraw(double value) {
+		if (balance < value) {
+			return false;
+		} else {
+			balance -= value;
+		}
+		balance -= value;
+		return false;
+		
 	}
 }
--- src/contabanco/brando/App.java
+++ src/contabanco/brando/App.java
@@ -1,22 +1,25 @@
 package contabanco.brando;
 
 public class App {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Ola mundo");
 		
+		// Criar uma conta (agencia, conta, nome)
+		//Limitar o nome = 12 caracteres
 		
-		//Criar uma conta (agencia,conta nome)
-		Account account = new Account(ag: "0001", cc: "1234", name: "Brando");
+		Account account = new Account("0001", "1234", "Brando");
 		
+		boolean succed = account.withDraw( 200.0);
+		if (!succed) {
+			System.out.println("Voce nao tem saldo suficiente para sacar");
+		}
 		
-		//Limite maximo de nome 12 caracteres
 		//Sacar valores
-		//Não pode sacar mais do que tem
+		// Não pode sacar mais do que tem
 		//Depositar
-		//Informar ao usuário as operações
-		
-		
+		//Informara para o usuário as operações (sauqe, deposito)
+
 	}
 
 }
